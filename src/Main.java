8fe10d227001236cd0d83329a99807a0159b8bd9
cesar <cesarcp@hotmail.es>
2024-11-09 14:27:31 +0000

    TicTacToe without diagonal

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class Main {
+    public static void game(){
+        Scanner sc = new Scanner(System.in);
+        int row = 0;
+        int col = 0;
+        Tictactoe game = new Tictactoe();
+        game.newGame();
+        game.showBoard();
+        while(game.isLine() == '_'){
+            try{
+                System.out.println("selecciona fila: ");
+                row = sc.nextInt() -1;
+                System.out.println("selecciona column: ");
+                col = sc.nextInt() -1;
+                if(game.isEmpty(row,col)){
+                    game.setToken(row,col);
+                    game.showBoard();
+                }else{
+                    System.out.println("La posicion seleccionada ya estaba ocupada");
+                }
+            }catch(Exception e){
+                System.out.println("Escribe un numero valido");
+            }
+        }
+        if(game.isLine() == 'O'){
+            System.out.println("Ganador: O");
+        }else{
+            System.out.println("Ganador: X");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int option = 0;
+        System.out.println("BIENVENIDO AL 3 EN RAYA");
+        do{
+            System.out.println("1.INICIAR NUEVA PARTIDA\n0.SALIR");
+            option = sc.nextInt();
+            if(option == 1){
+                game();
+            }
+        }while(option != 0);
+
+    }
+}
---
+++ src/Tictactoe.java
@@ -0,0 +1,99 @@
+public class Tictactoe {
+    private boolean player; // false es la X
+    private char board [][] = new char [3][3];
+
+    public Tictactoe() {
+        this.player = false;
+    }
+
+    //This function sets up the board for a new game
+    public void newGame(){
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                board[i][j] = '_';
+            }
+        }
+    }
+
+    //Shows the board
+    public void showBoard(){
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                System.out.print(board[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    //Find is the pos is eligible for set a new token
+    public boolean isEmpty(int row, int col){
+        return board[row][col] == '_';
+    }
+
+    //Set a token in the chosen position
+    public void setToken (int row, int col){
+        if (player){
+            board[row][col] = 'O';
+            player = false;
+        }else{
+            board[row][col] = 'X';
+            player = true;
+        }
+    }
+
+    private char isVerticalLine(){
+        for(int i = 0; i < 3; i++){
+            int count = 0;
+            for(int j = 0; j < 3; j++){
+                if(board[j][i] == 'X'){count++;}
+            }
+            if(count == 3){
+                return 'X';
+            }
+        }
+        for(int i = 0; i < 3; i++){
+            int count = 0;
+            for(int j = 0; j < 3; j++){
+                if(board[j][i] == 'O'){count++;}
+            }
+            if(count == 3){
+                return 'O';
+            }
+        }
+        return '_';
+    }
+
+    private char isHorizontalLine(){
+        for(int i = 0; i < 3; i++){
+            int count = 0;
+            for(int j = 0; j < 3; j++){
+                if(board[i][j] == 'X'){count++;}
+            }
+            if(count == 3){
+                return 'X';
+            }
+        }
+        for(int i = 0; i < 3; i++){
+            int count = 0;
+            for(int j = 0; j < 3; j++){
+                if(board[i][j] == 'O'){count++;}
+            }
+            if(count == 3){
+                return 'O';
+            }
+        }
+        return '_';
+    }
+
+
+    public char isLine (){
+
+       if(isHorizontalLine() != '_'){
+           return isHorizontalLine();
+       }
+       if(isVerticalLine() != '_'){
+           return isVerticalLine();
+       }
+       return '_';
+    }
+}
